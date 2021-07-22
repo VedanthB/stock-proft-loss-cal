c89c3403586f6de4afee6dcba291065fee557d3a
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-23 04:52:13 +0530

    css + html styling

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const form = document.forms[0];
 const inputs = document.querySelectorAll(".input");
 const output = document.querySelector(".output");
+// for the output bg
+const contentDiv = document.querySelector(".displayFlex");
+const cols = document.querySelectorAll(".col");
 
 form.addEventListener("submit", function checkHandler(e) {
   e.preventDefault();
+  cols[0].classList.remove("transperantBg");
+  cols[1].classList.remove("transperantBg");
+  contentDiv.classList.remove("sadTheme");
+  contentDiv.classList.remove("happyTheme");
+
   let CP = inputs[0].value;
   let Qty = inputs[1].value;
   let SP = inputs[2].value;
   console.log(CP, Qty, SP);
 
+  // false--eyy Not - A - Number then exec this == is a number
   if (!isNaN(CP) && !isNaN(Qty) && !isNaN(SP)) {
     CP = Number(CP);
     Qty = Number(Qty);
@@ -17,14 +26,29 @@ form.addEventListener("submit", function checkHandler(e) {
       //loss
       if (CP > SP) {
         const loss = ((CP - SP) * Qty).toFixed(2);
+        // percentage
         const lossPer = (((CP - SP) * 100) / CP).toFixed(2);
         output.innerHTML = `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem">You lost ${lossPer}%. Your total loss is ₹${loss}</div>`;
+        // if loss percentage is less then 50 then
+
+        if (lossPer > 50) {
+          cols[0].classList.add("transperantBg");
+          cols[1].classList.add("transperantBg");
+          contentDiv.classList.add("sadTheme");
+        }
       }
       //profit
       else {
         const profit = ((SP - CP) * Qty).toFixed(2);
         const profitPer = (((SP - CP) * 100) / CP).toFixed(2);
         output.innerHTML = `<div style="background-color: rgb(255, 255,255,0.2); padding: 1rem">You gained ${profitPer}%. Your total profit is ₹${profit}</div>`;
+
+        //if profit percentage is more than 50
+        if (profitPer > 50) {
+          cols[0].classList.add("transperantBg");
+          cols[1].classList.add("transperantBg");
+          contentDiv.classList.add("happyTheme");
+        }
       }
     } else {
       //error
